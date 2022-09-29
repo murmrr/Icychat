c47deacc553a52f5267b820569c96d9957f3fe9a
themorlock <saaketh.vangati@gmail.com>
2022-09-29 14:47:45 -0500

    changed canister id

--- config/index.ts
+++ config/index.ts
@@ -1,6 +1,6 @@
 const IS_LOCAL = !!__DEV__;
 
-const BACKEND_CANISTER_ID = "h7awn-gyaaa-aaaam-qa4va-cai";
+const BACKEND_CANISTER_ID = "os5rm-ziaaa-aaaag-qavuq-cai";
 
 const IC_HOST = "https://boundary.ic0.app/";
 
