8e6d438a63b7602795418760f3668928d18ec512
themorlock <saaketh.vangati@gmail.com>
2022-10-05 18:53:29 -0500

    before new color theme

--- components/ProfilePictureStack/ProfilePictureStack.js
+++ components/ProfilePictureStack/ProfilePictureStack.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { View } from "react-native";
 import CustomProfilePicture from "../CustomProfilePicture/CustomProfilePicture";
 
+const ProfilePictureStackMultiple = ({ principals, style }) => {
+  return (
+    <CustomProfilePicture principal={principals[0]} style={style} />
+  )
+};
+
 const ProfilePictureStack = ({ principals, style }) => {
-  return <CustomProfilePicture principal={principals[0]} style={style} />;
+  return principals.length == 1 ? <CustomProfilePicture principal={principals[0]} style={style} /> : <ProfilePictureStackMultiple principals={principals} style={style} />
 };
 
 export default ProfilePictureStack;
